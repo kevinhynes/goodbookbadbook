c99d043219650b75a745ec6116fdbcc4d2e7659e
kevin <khynes57@outlook.com>
2020-05-28 14:55:09 -0400

    Fixed issue with import.py, books database now on Heroku

--- import.py
+++ import.py
@@ -1,22 +1,24 @@
 import csv, os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine(os.getenv("DATABASE_URL"))
-conn = engine.connect()
+db = scoped_session(sessionmaker(bind=engine))
 
 
 def create_books_table():
-    conn.execute("CREATE TABLE IF NOT EXISTS Books(Title TEXT, Author TEXT, Year INT, ISBN INT)")
+    db.execute("DROP TABLE IF EXISTS Books")
+    db.execute("CREATE TABLE IF NOT EXISTS Books(Title TEXT, Author TEXT, Year INT, ISBN TEXT)")
+    db.commit()
 
 
 def import_data():
     f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
-        conn.execute("INSERT INTO Books (Title, Author, Year, ISBN) VALUES (:title, :author, :year, :isbn)",
+        db.execute("INSERT INTO Books (Title, Author, Year, ISBN) VALUES (:title, :author, :year, :isbn)",
                      {"title": title, "author": author, "year": year, "isbn": isbn})
-    print(f"Added {title}")
-
+    db.commit()
 
 if __name__ == "__main__":
     create_books_table()
