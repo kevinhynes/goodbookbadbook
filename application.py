8c928e555c58ed64a40d494440b5b9f4b788f011
kevin <khynes57@outlook.com>
2020-05-31 13:33:12 -0400

    Login / Sign Up / Logout working

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import generate_password_hash, check_password_hash
+import functools
 
 app = Flask(__name__)
 
@@ -22,21 +23,37 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+def login_required(route_func):
+    @functools.wraps(route_func)  # returns wrapper as route_func (?).
+    def wrapper(*args, **kwargs):
+        if "logged_in" in session:
+            return route_func(*args, **kwargs)
+        else:
+            flash("You must log in to use GoodBookBadBook.", category="error")
+            return redirect(url_for('login'))
+    return wrapper
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=session.get("logged_in", False))
 
+@app.route("/main")
+def main():
+    return render_template("main.html", logged_in=session.get("logged_in", False))
 
 @app.route("/books")
+@login_required  # calls wrapper() with arguments to books().
 def books():
     books = db.execute("SELECT * from Books").fetchall()
-    return render_template("books.html", books=books)
+    return render_template("books.html", books=books, logged_in=session.get("logged_in", False))
 
 
 @app.route("/authors")
+@login_required  # calls wrapper() with arguments to authors().
 def authors():
     books = db.execute("SELECT * from Books").fetchall()
-    return render_template("authors.html", books=books)
+    return render_template("authors.html", books=books, logged_in=session.get("logged_in", False))
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -62,25 +79,48 @@ def validate_registration(username, pwd, repeat_pwd):
                                {"username": username}).fetchone()
     print(f"validate_registration {existing_user}")
     if existing_user:
-        print(f"Username unavailable.", flush=True)
         flash("Username unavailable.", category="error")
         return False
+    if len(username) < 3:
+        flash("Username must be at least 3 characters", category="error")
+        return False
     if pwd != repeat_pwd:
-        print(f"Passwords do not match.", flush=True)
         flash("Passwords do not match.", category="error")
+        return False
+    if len(pwd) < 3:
+        flash("Password must be at least 3 characters.", category="error")
         return False
     return True
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    username = request.form.get("username")
+    pwd = request.form.get("pwd")
+    if request.method == "POST":
+        existing_user = db.execute("SELECT * from Users WHERE Username = :username",
+                                   {"username": username}).fetchone()
+        print(f"login() POST, existing_user: {existing_user}", flush=True)
+        if not existing_user:
+            flash("No user found. Please sign up first.", category="error")
+            return redirect(url_for('signup'))
+        if not check_password_hash(existing_user.hash, pwd):
+            flash("Incorrect password.", category="error")
+        else:
+            session["logged_in"] = True
+            flash("Login successful. Thank you for using GoodBookBadBook.", category="success")
+            print(f"login() POST")
+            return redirect(url_for('main'))
     return render_template("login.html")
 
 
-@app.route("/test")
-def test():
-    flash("TESTINGGG")
-    return render_template("test.html")
+@app.route("/logout")
+def logout():
+    session.pop("_flashes", None)
+    session.pop("logged_in", None)
+    flash("You have successfully been logged out. Come back soon!", category="success")
+    return render_template("logout.html", logged_in=session.get("logged_in", False))
+
 
 
 goodreads_api_key = "SdD1S2KiCOWfPmTLyhbrA"
