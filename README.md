6a843cb9b5c5f1e65b7b28428b95233e9512ed60
kevin <khynes57@outlook.com>
2020-05-31 18:26:12 -0400

    Login Successful flash message will not go away. Why?

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Web Programming with Python and JavaScript
 
 To run from git bash:
 
--export FLASK_APP=application.py
--export DATABASE_URL=heroku_URI
--FLASK_ENV=development flask run
+`export FLASK_APP=application.py`
+
+`export FLASK_DEBUG=1`   (optional)
+
+`flask run`
--- application.py
+++ application.py
@@ -10,8 +10,11 @@ app = Flask(__name__)
 
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+DATABASE_URL = "postgres://xqmbmqkcvpavvs:17f6f6f690bb777e4d484e3ee441e3b4d8c88591d239a9beb49aca4599863b97@ec2-52-70-15-120.compute-1.amazonaws.com:5432/dfg72kp1ckutig"
+app.config["DATABASE_URL"] = DATABASE_URL
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -19,13 +22,15 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(DATABASE_URL)
 db = scoped_session(sessionmaker(bind=engine))
 
 
 def login_required(route_func):
     @functools.wraps(route_func)  # returns wrapper as route_func (?).
     def wrapper(*args, **kwargs):
+        print(f"wrapper flashes: {session.get('_flashes', [])}", flush=True)
         if "logged_in" in session:
             return route_func(*args, **kwargs)
         else:
@@ -36,22 +41,38 @@ def login_required(route_func):
 
 @app.route("/")
 def index():
+    print(f"index flashes: {session.get('_flashes', [])}", flush=True)
     return render_template("index.html", logged_in=session.get("logged_in", False))
 
+
 @app.route("/main")
+@login_required
 def main():
+    print(f"main flashes: {session.get('_flashes', [])}", flush=True)
     return render_template("main.html", logged_in=session.get("logged_in", False))
+
 
 @app.route("/books")
 @login_required  # calls wrapper() with arguments to books().
 def books():
+    print(f"books flashes: {session.get('_flashes', [])}", flush=True)
     books = db.execute("SELECT * from Books").fetchall()
     return render_template("books.html", books=books, logged_in=session.get("logged_in", False))
+
+
+@app.route("/books/<int:book_id>")
+@login_required  # calls wrapper() with arguments to books().
+def book(book_id):
+    print(f"book flashes: {session.get('_flashes', [])}", flush=True)
+    book = db.execute("SELECT * from Books WHERE id = :book_id", {"book_id": book_id}).fetchone()
+    print(f"book() /books/book_id {book}", flush=True)
+    return render_template("book.html", book=book, logged_in=session.get("logged_in", False))
 
 
 @app.route("/authors")
 @login_required  # calls wrapper() with arguments to authors().
 def authors():
+    print(f"authors flashes: {session.get('_flashes', [])}", flush=True)
     books = db.execute("SELECT * from Books").fetchall()
     return render_template("authors.html", books=books, logged_in=session.get("logged_in", False))
 
@@ -61,11 +82,10 @@ def signup():
     username = request.form.get("username")
     pwd = request.form.get("pwd")
     repeat_pwd = request.form.get("repeat_pwd")
+    print(f"signup flashes: {session.get('_flashes', [])}", flush=True)
     if request.method == "POST":
-        print(f"POST {username}", flush=True)
         if validate_registration(username, pwd, repeat_pwd):
             hash = generate_password_hash(pwd)
-            print(f"registration validated {username, pwd, hash}", flush=True)
             db.execute("INSERT INTO Users(Username, Hash) VALUES (:username, :hash)",
                        {"username": username, "hash": hash})
             db.commit()
@@ -77,7 +97,7 @@ def signup():
 def validate_registration(username, pwd, repeat_pwd):
     existing_user = db.execute("SELECT * from Users WHERE Username = :username",
                                {"username": username}).fetchone()
-    print(f"validate_registration {existing_user}")
+    print(f"validate_registration flashes: {session.get('_flashes', [])}", flush=True)
     if existing_user:
         flash("Username unavailable.", category="error")
         return False
@@ -97,26 +117,28 @@ def validate_registration(username, pwd, repeat_pwd):
 def login():
     username = request.form.get("username")
     pwd = request.form.get("pwd")
+    print(f"login flashes: {session.get('_flashes', [])}", flush=True)
+
     if request.method == "POST":
         existing_user = db.execute("SELECT * from Users WHERE Username = :username",
                                    {"username": username}).fetchone()
-        print(f"login() POST, existing_user: {existing_user}", flush=True)
         if not existing_user:
             flash("No user found. Please sign up first.", category="error")
             return redirect(url_for('signup'))
         if not check_password_hash(existing_user.hash, pwd):
             flash("Incorrect password.", category="error")
+            return
         else:
             session["logged_in"] = True
             flash("Login successful. Thank you for using GoodBookBadBook.", category="success")
-            print(f"login() POST")
             return redirect(url_for('main'))
     return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    session.pop("_flashes", None)
+    # session.pop("_flashes", None)
+    print(f"logout flashes: {session.get('_flashes', [])}", flush=True)
     session.pop("logged_in", None)
     flash("You have successfully been logged out. Come back soon!", category="success")
     return render_template("logout.html", logged_in=session.get("logged_in", False))
